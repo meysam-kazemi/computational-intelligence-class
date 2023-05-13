5644da458fccd80f74a19300ee1175db8c624eba
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-14 02:03:37 +0330

    model before training

--- project1.jl
+++ project1.jl
@@ -1,4 +1,6 @@
-# SEED
+# Import librarie
+using Plots # Plots library
+## SEED
 import Random
 Random.seed!(1234)
 
@@ -7,15 +9,16 @@ Random.seed!(1234)
 a , b , d ,e = 5 , 6 , 9 , 0;
 function Generate_data(a,b,d,e)
     data = zeros(3);
-    data[1] = rand(); # Random number for x1
+    data[1] = randn(); # Random number for x1
     data[2] = rand(); # Random number for x2
     # We put x3 so that it applies in the equation
     data[3] = -(a*data[1] + b*data[2] + e) / d; 
     return data
 end
 
-X = zeros(Float64,1000,3); # Save data in X
-for i in 1:1000
+n_samples = 200
+X = zeros(Float64,n_samples,3); # Save data in X
+for i in 1:n_samples
     X[i,:] = Generate_data(a,b,d,e);
 end
 
@@ -23,10 +26,15 @@ end
 
 # Build a model with random weights
 W = 0.001 * rand(3,3);
-Y = zeros(1000,3);
+Y = zeros(n_samples,3);
 for (i,row) in enumerate(eachrow(X))
     Y[i,:] = W*row;
 end
+
+Plots.scatter(Y[:,1],Y[:,2],Y[:,3],color=:red,label = :"y")
+Plots.scatter!(X[:,1],X[:,2],X[:,3],color=:blue,label="x")
+
+
 
 
 
