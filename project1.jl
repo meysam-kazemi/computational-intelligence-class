b3c27144c764a2f1acc95142537a43ccd48dd0d5
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-13 21:13:13 +0330

    generate data

--- project1.jl
+++ project1.jl
@@ -1,5 +1,12 @@
 # Generate data
 # ax1 + bx2 + dx3 + e = 0
 a , b , d ,e = 5 , 6 , 9 , 0;
+function Generate_data(a,b,d,e)
+    data = zeros(3)
+    data[1] = rand()
+    data[2] = rand()
+    data[3] = -(a*data[1] + b*data[2] + e) / d    
+end
+
 
 
