03df621d491f5508c5bffcba8ea31bcd50ec9589
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-14 00:31:04 +0330

    add comments

--- project1.jl
+++ project1.jl
@@ -7,15 +7,17 @@ Random.seed!(1234)
 a , b , d ,e = 5 , 6 , 9 , 0;
 function Generate_data(a,b,d,e)
     data = zeros(3);
-    data[1] = rand();
-    data[2] = rand();
-    data[3] = -(a*data[1] + b*data[2] + e) / d;
+    data[1] = rand(); # Random number for x1
+    data[2] = rand(); # Random number for x2
+    # We put x3 so that it applies in the equation
+    data[3] = -(a*data[1] + b*data[2] + e) / d; 
     return data
 end
 
-X = zeros(Float64,1000,3);
+X = zeros(Float64,1000,3); # Save data in X
 for i in 1:1000
     X[i,:] = Generate_data(a,b,d,e);
 end
+
 
 
