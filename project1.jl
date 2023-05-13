def9ef6bc991aa8e69bc10e92979d1406631894a
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-14 00:23:44 +0330

    generate dataset and save them in X

--- project1.jl
+++ project1.jl
@@ -2,11 +2,16 @@
 # ax1 + bx2 + dx3 + e = 0
 a , b , d ,e = 5 , 6 , 9 , 0;
 function Generate_data(a,b,d,e)
-    data = zeros(3)
-    data[1] = rand()
-    data[2] = rand()
-    data[3] = -(a*data[1] + b*data[2] + e) / d    
+    data = zeros(3);
+    data[1] = rand();
+    data[2] = rand();
+    data[3] = -(a*data[1] + b*data[2] + e) / d;
+    return data
 end
 
+X = zeros(Float64,1000,3);
+for i in 1:1000
+    X[i,:] = Generate_data(a,b,d,e);
+end
 
 
