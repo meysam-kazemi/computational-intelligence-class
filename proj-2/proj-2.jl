8cd34d6e74c156a74cdaa43bcbb38aad3a23599b
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-20 05:45:04 +0330

    debug ordered crossover

--- proj-2/proj-2.jl
+++ proj-2/proj-2.jl
@@ -63,13 +63,13 @@ function ordered_crossover(parent1, parent2)
     idx = 1
     for i in 1:NUM_CITIES
         if child[i] == -1
-            while parent2[idx] in child
+            while parent2[idx] in child && idx < length(parent2)
                 idx += 1
             end
             child[i] = parent2[idx]
         end
     end
-
+    child[1] = parent1[1]
     return child
 end
 
@@ -79,6 +79,7 @@ function mutate(path)
         idx1, idx2 = sort(randperm(NUM_CITIES)[1:2])
         path[idx1], path[idx2] = path[idx2], path[idx1]
     end
+    path[1] = 1
     return path
 end
 
@@ -86,7 +87,8 @@ end
 function genetic_algorithm()
     population = generate_population(NUM_CITIES, POPULATION_SIZE)
 
-    for _ in 1:MAX_GENERATIONS
+    for i in 1:MAX_GENERATIONS
+        print("\r $(Int(round(100i/MAX_GENERATIONS))) %")
         new_population = []
         while length(new_population) < POPULATION_SIZE
             parent1, parent2 = tournament_selection(population)
@@ -100,6 +102,7 @@ function genetic_algorithm()
 
     best_path = population[argmin([total_distance(p) for p in population])]
     best_distance = total_distance(best_path)
+    print("\n")
     return best_path, best_distance
 end
 
@@ -112,8 +115,10 @@ println("Best distance: ", best_distance)
 
 theme(:default)
 scatter(points[:,1],points[:,2],points[:,3],color=:red,
-    camera=(20,20),markerstrokewidth=0)
+    camera=(20,20),markerstrokewidth=0,xlabel="x",
+    ylabel="y",zlabel="z")
 plots_path = points[best_path,:]
 plot!(plots_path[:,1],plots_path[:,2],plots_path[:,3],linestyle=:dash,
     linewidth=1)
+plot!(points[best_path[1],1,:],points[best_path[1],2,:],points[best_path[1],3,:],color=:blue)
 title!("Project - 2")
