6ec9d6440338c0a36195eb1005817fb113519f4a
meysam-kazemi <meisam.daysu@gmail.com>
2023-06-01 04:47:40 +0330

    define epochs

--- proj-3/proj-3.jl
+++ proj-3/proj-3.jl
@@ -1,6 +1,8 @@
 # Define the number of inputs and outputs.
 INPUT = 2;
 OUTPUT = 3;
+# Define epochs
+EPOCHS = 100;
 # Activation function -> Unit step function
 unit_step(x) = @. x >= 0 ? 1 : 0
 # Generate Data
@@ -8,4 +10,6 @@ X = [0 0;0 1;1 0;1 1];
 Y_or = [0;1;1;1];
 Y_and = [0;0;01];
 Y_xor = [0;1;1;0];
+
+
 
