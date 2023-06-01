21acc8627f557483c6f3f9b6875b58a2300ccbb9
meysam-kazemi <meisam.daysu@gmail.com>
2023-06-01 04:43:39 +0330

    Generate data

--- proj-3/proj-3.jl
+++ proj-3/proj-3.jl
@@ -3,7 +3,8 @@ INPUT = 2;
 OUTPUT = 3;
 # Activation function -> Unit step function
 unit_step(x) = @. max(0,sign(x));
-
-t = -5:0.01:5;
-import Plots
-Plots.plot(t,unit_step(t))
+# Generate Data
+X = [0 0;0 1;1 0;1 1];
+Y_or = [0;1;1;1];
+Y_and = [0;0;01];
+Y_xor = [0;1;1;0];
