7323f709e155507927e3de9e7bc4446ee37eabc3
meysam-kazemi <meisam.daysu@gmail.com>
2023-06-01 04:38:39 +0330

    edit activation func and test it

--- proj-3/proj-3.jl
+++ proj-3/proj-3.jl
@@ -2,4 +2,8 @@
 INPUT = 2;
 OUTPUT = 3;
 # Activation function -> Unit step function
-unit_step(x) = max(0,sign(x));
+unit_step(x) = @. max(0,sign(x));
+
+t = -5:0.01:5;
+import Plots
+Plots.plot(t,unit_step(t))
