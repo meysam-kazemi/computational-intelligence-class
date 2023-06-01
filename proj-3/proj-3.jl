50ec4465b8797936ab2ea4f6753140156f93e2fe
meysam-kazemi <meisam.daysu@gmail.com>
2023-06-01 06:17:33 +0330

    start write update function

--- proj-3/proj-3.jl
+++ proj-3/proj-3.jl
@@ -8,8 +8,8 @@ const OUTPUT = 3;
 const EPOCHS = 100;
 const LR = 0.001;
 # Weights
-const W = 0.001*rand(OUTPUT,INPUT);
-const bias = 0;
+global W = 0.001*rand(OUTPUT,INPUT);
+global bias = 0;
 # Activation function -> Unit step function
 unit_step(x) = @. x >= 0 ? 1 : 0 # If it was smaller than 0, return 0 and otherwise 1
 # Generate Data
@@ -26,7 +26,11 @@ function layer(x)
     end
     return output_
 end
+# Update the Weights
+function update(x,y)
+    global W,bias
+    
+end
 
-a = layer(X)
-
+update(X,Y)
 
