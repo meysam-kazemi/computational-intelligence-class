341aad02d2724a879814347b1698640851e05a70
meysam-kazemi <meisam.daysu@gmail.com>
2023-06-01 04:46:41 +0330

    edit activation function

--- proj-3/proj-3.jl
+++ proj-3/proj-3.jl
@@ -2,9 +2,10 @@
 INPUT = 2;
 OUTPUT = 3;
 # Activation function -> Unit step function
-unit_step(x) = @. max(0,sign(x));
+unit_step(x) = @. x >= 0 ? 1 : 0
 # Generate Data
 X = [0 0;0 1;1 0;1 1];
 Y_or = [0;1;1;1];
 Y_and = [0;0;01];
 Y_xor = [0;1;1;0];
+
